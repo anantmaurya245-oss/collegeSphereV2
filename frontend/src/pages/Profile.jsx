5e1dd234ea85edf7d756647f3ef7138c4f832b99
anantmaurya245-oss <anantmaurya245@gmail.com>
2026-08-06 12:01:54 +0530

    Complete Sprint 8 - Edit Profile with Image Upload

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,12 +5,31 @@ export default function Profile() {
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // NEW STATE
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+  first_name: "",
+  last_name: "",
+  bio: "",
+  college: "",
+  department: "",
+  year: "",
+});
+
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         const response = await api.get("auth/me/");
-
         setProfile(response.data);
+        setFormData({
+          first_name: response.data.first_name || "",
+          last_name: response.data.last_name || "",
+          bio: response.data.bio || "",
+          college: response.data.college || "",
+          department: response.data.department || "",
+          year: response.data.year || "",
+        });
       } catch (error) {
         console.error("Profile Error:", error);
       } finally {
@@ -40,6 +59,45 @@ export default function Profile() {
   const imageUrl = profile.profile_picture
     ? profile.profile_picture.replace("127.0.0.1", "localhost")
     : "https://placehold.co/200x200?text=User";
+  const handleSave = async () => {
+    try {
+      const data = new FormData();
+
+      data.append("first_name", formData.first_name);
+      data.append("last_name", formData.last_name);
+      data.append("bio", formData.bio);
+      data.append("college", formData.college);
+      data.append("department", formData.department);
+      data.append("year", formData.year);
+    if (selectedImage) {
+      data.append("profile_picture", selectedImage);
+    }
+
+      const response = await api.put(
+        "auth/me/",
+        data,
+        {
+          headers: {
+             "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setProfile(response.data);
+
+      setIsEditing(false);
+
+      alert("Profile Updated Successfully!");
+    } catch (error) {
+      console.error(error);
+
+      if (error.response) {
+        console.log(error.response.data);
+      }
+
+      alert("Failed to update profile.");
+    }
+  };
 
   return (
     <div className="max-w-4xl mx-auto mt-10">
@@ -82,46 +140,162 @@ export default function Profile() {
           <div className="grid md:grid-cols-2 gap-6">
 
             <div>
-              <h2 className="font-bold text-lg">
-                Bio
-              </h2>
-
-              <p className="text-gray-700">
-                {profile.bio || "No bio added"}
-              </p>
+              <h2 className="font-bold text-lg">Bio</h2>
+              <p>{profile.bio || "No bio added"}</p>
             </div>
 
             <div>
-              <h2 className="font-bold text-lg">
-                College
-              </h2>
-
-              <p>
-                {profile.college || "Not Added"}
-              </p>
+              <h2 className="font-bold text-lg">College</h2>
+              <p>{profile.college || "Not Added"}</p>
             </div>
 
             <div>
-              <h2 className="font-bold text-lg">
-                Department
-              </h2>
-
-              <p>
-                {profile.department || "Not Added"}
-              </p>
+              <h2 className="font-bold text-lg">Department</h2>
+              <p>{profile.department || "Not Added"}</p>
             </div>
 
             <div>
-              <h2 className="font-bold text-lg">
-                Year
-              </h2>
-
-              <p>
-                {profile.year || "Not Added"}
-              </p>
+              <h2 className="font-bold text-lg">Year</h2>
+              <p>{profile.year || "Not Added"}</p>
             </div>
 
           </div>
+
+          {/* NEW BUTTON */}
+          <div className="mt-8">
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg"
+            >
+              Edit Profile
+            </button>
+          </div>
+
+          {/* TEMPORARY MESSAGE */}
+          {isEditing && (
+  <div className="mt-8 space-y-4">
+
+    <input
+      type="text"
+      placeholder="First Name"
+      value={formData.first_name}
+      onChange={(e) =>
+        setFormData({
+          ...formData,
+          first_name: e.target.value,
+        })
+      }
+      className="w-full border rounded-lg p-3"
+    />
+
+    <input
+      type="text"
+      placeholder="Last Name"
+      value={formData.last_name}
+      onChange={(e) =>
+        setFormData({
+          ...formData,
+          last_name: e.target.value,
+        })
+      }
+      className="w-full border rounded-lg p-3"
+    />
+
+    <textarea
+      placeholder="Bio"
+      value={formData.bio}
+      onChange={(e) =>
+        setFormData({
+          ...formData,
+          bio: e.target.value,
+        })
+      }
+      className="w-full border rounded-lg p-3"
+    />
+
+    <input
+      type="text"
+      placeholder="College"
+      value={formData.college}
+      onChange={(e) =>
+        setFormData({
+          ...formData,
+          college: e.target.value,
+        })
+      }
+      className="w-full border rounded-lg p-3"
+    />
+
+    <input
+      type="text"
+      placeholder="Department"
+      value={formData.department}
+      onChange={(e) =>
+        setFormData({
+          ...formData,
+          department: e.target.value,
+        })
+      }
+      className="w-full border rounded-lg p-3"
+    />
+
+    <input
+      type="number"
+      placeholder="Year"
+      value={formData.year}
+      onChange={(e) =>
+        setFormData({
+          ...formData,
+          year: e.target.value,
+        })
+      }
+      className="w-full border rounded-lg p-3"
+    />
+
+    <div className="flex gap-4">
+       <div className="space-y-3">
+
+  <label className="font-semibold">
+    Profile Picture
+  </label>
+
+  <input
+    type="file"
+    accept="image/*"
+    onChange={(e) => {
+      setSelectedImage(e.target.files[0]);
+    }}
+    className="w-full"
+  />
+
+  {selectedImage && (
+    <img
+      src={URL.createObjectURL(selectedImage)}
+      alt="Preview"
+      className="w-32 h-32 rounded-full object-cover border"
+    />
+  )}
+
+</div>
+
+      <button
+        onClick={handleSave}
+        className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-lg"
+    >   
+      Save Changes
+    </button>
+
+      <button
+        onClick={() => setIsEditing(false)}
+        className="bg-red-500 text-white px-6 py-2 rounded-lg"
+      >
+        Cancel
+      </button>
+
+    </div>
+
+  </div>
+)}
 
         </div>
 
